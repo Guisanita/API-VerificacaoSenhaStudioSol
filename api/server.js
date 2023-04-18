435eab70809c6eaff96550ac2c16b4bcfb9fd885
Guilherme Sanita <guilherme.ssanita@gmail.com>
2023-04-17 22:40:03 -0300

    testando mais uma vez

--- api/server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./index');
-const routes = require("../src/routes/password.routes");
-
-app.use(express.json());
-app.use(routes);
-
-app.listen(8080);
---
+++ server.js
@@ -0,0 +1,7 @@
+const app = require('express');
+const routes = require("./src/routes/password.routes");
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(process.env.PORT);
