75cd90d9e8501c88f08f0ffaea9e20a609bb9e9a
Guilherme Sanita <guilherme.ssanita@gmail.com>
2023-04-17 22:52:25 -0300

    Ajustes deploy

--- server.js
+++
@@ -1,7 +0,0 @@
-const app = require('express')();
-const routes = require("./src/routes/password.routes");
-
-app.use(express.json());
-app.use(routes);
-
-app.listen(process.env.PORT);
---
+++ src/server.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const cors = require('cors');
+const express = require('express');
+const routes = require("./routes/password.routes");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+
+app.listen(process.env.PORT);
